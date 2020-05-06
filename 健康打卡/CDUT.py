71d24d2d3fb28f63d414e77c2acf0c5672d03370
JoJoJoinme <49182531+JoJoJoinme@users.noreply.github.com>
2020-05-06 17:07:32 +0800

    提交健康打卡相关内容

---
+++ 健康打卡/CDUT.py
@@ -0,0 +1,88 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import paramunittest
+import yaml
+
+# 将括号内的地址修改为你的data.yml存放地址
+with open(r'D:\python3\健康打卡\data.yml', encoding='utf-8')as fp_stream:
+    datalist = yaml.load(fp_stream, Loader=yaml.FullLoader)
+
+
+@paramunittest.parametrized(*datalist)
+class CdutHealth(unittest.TestCase):
+    def setParameters(self, user, pwd, province=None, city=None, county=None, address=None):
+        self.username = user
+        self.password = pwd
+        self.province = province
+        self.city = city
+        self.county = county
+        self.address = address
+
+    def setUp(self):
+        # 用哪个就把另一个注释掉
+        self.driver = webdriver.Chrome('F:\Chrome Driver\chromedriver_win32\chromedriver.exe')  # 括号内为你的Chromedriver存放路径
+        self.driver = webdriver.Edge('F:\Edge Driver\edgedriver_win64\msedgedriver.exe')       # 括号内为你的MicrosoftEdge driver存放路径
+
+    def test_python(self):
+        driver = self.driver
+        driver.get(
+            'https://authserver.cdut.edu.cn/authserver/login?service=https%3A%2F%2Fehall.cdut.edu.cn%3A443%2Flogin%3Fservice%3Dhttps%3A%2F%2Fehall.cdut.edu.cn%2Fnew%2Findex.html')
+        username = driver.find_element_by_id("username")
+        username.send_keys(self.username)
+        passwd = driver.find_element_by_id("password")
+        passwd.send_keys(self.password)
+        driver.find_element_by_class_name('dl-button').click()
+        time.sleep(3)
+        driver.find_element_by_class_name('ampHeaderSearchFlag').click()
+        element = driver.find_element_by_id('ampServiceSearchInput')
+        element.clear()
+        element.send_keys("学生健康状况信息采集系统")
+        element.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        time.sleep(2)  # 等待浏览器加载搜索元素
+        health = driver.find_element_by_class_name('amp-service-search-color')
+        health.click()
+        time.sleep(2)  # 休眠两秒，等待浏览器加载表格
+        allwindows = driver.window_handles
+        curwinodw = driver.current_window_handle
+        if (len(allwindows) > 1 and curwinodw == allwindows[0]):
+            driver.switch_to.window(allwindows[1])
+        iframe1 = driver.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div[2]/div/table/tbody/tr/td[2]/div[2]/div/iframe')  # 寻找第一个内嵌表格
+        driver.switch_to.frame(iframe1)  # driver切换到第一个表格
+        iframe2 = driver.find_element_by_xpath(
+            '/html/body/div[2]/div[2]/table/tbody/tr/td[2]/div[2]/div/iframe')  # 寻找第二个内嵌表格
+        driver.switch_to.frame(iframe2)  # 切换第二个表格
+        driver.find_element_by_id('mini-2$0').click()  # 点击确认按钮
+        print("开始寻找是否有黑龙江旅居史框")
+        driver.find_element_by_id('mini-36$ck$1').click()  # 点击否
+        print("开始寻找是否有小区隔离史")
+        driver.find_element_by_id('mini-66$ck$1').click() # 点击否
+        print("开始寻找体温属性")
+        temprature = driver.find_element_by_id('TW$text')  # 寻找体温属性
+        temprature.send_keys('36.5')  # 输入体温
+        print('填写当前地址')
+        driver.find_element_by_id('SSQ21$text').send_keys(self.province)  # 填写省
+        driver.find_element_by_id('mini-49').click()  # 点击下拉栏
+        driver.find_element_by_id('SSQ22$text').send_keys(self.city)  # 填写市
+        driver.find_element_by_id('mini-53').click()  # 点击市的下拉栏
+        driver.find_element_by_id('SSQ23$text').send_keys(self.county)
+        driver.find_element_by_id('mini-57').click()  # 点击县的下拉栏
+        driver.find_element_by_id('SZDXXDZ$text').send_keys(self.address)
+        driver.switch_to.default_content()  # 切换回顶层表单
+        driver.switch_to.frame(iframe1)  # 切换为第一层表单
+        driver.find_element_by_id('sendBtn').click()  # 寻找提交按钮并点击
+        print('finished')
+        time.sleep(1)
+        element = driver.find_element_by_id('mini-17')  # 寻找弹出页面框的确认按钮
+        element.click()
+        print('Another test')
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
---
+++ 健康打卡/README.md
@@ -0,0 +1,71 @@
+#### CDUT健康打卡自动化脚本使用指南
+
+#### 环境要求：
+
+系统：Windows 10
+
+Python版本：3.6+
+
+#### 使用方法：
+
+1. 将文件下载到电脑任意位置并切换到该目录
+   在当前目录下按 shift+鼠标右键 打开终端
+   然后pip install -r requirements.txt
+   
+2. 确定你要使用哪个浏览器，及对应的版本。下载相应的Driver
+
+   我在下面放上我测试过的浏览器的Driver下载地址
+
+   Microsoft Edge:https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+
+   Chrome:https://sites.google.com/a/chromium.org/chromedriver/downloads
+   
+3. 在**data.yml**文件中按照示例填上你的个人信息，注意用单引号框起来你的值（你也可以填多个人的信息，将会按填写顺序执行打卡)
+
+4. 打开CDUT.py文件，将第9行括号内的路径改为你的**data.yml**文件存放路径
+
+   ```python
+   with open(r'D:\python3\健康打卡\data.yml', encoding='utf-8')as fp_stream:
+   ```
+   
+   在第25，26行，如果用Microsoft Edge就将另一个注释掉。反之，亦然。
+   
+   ```python
+   self.driver = webdriver.Chrome('F:\Chrome Driver\chromedriver_win32\chromedriver.exe')  # 括号内为你的Chromedriver存放路径
+   self.driver = webdriver.Edge('F:\Edge Driver\edgedriver_win64\msedgedriver.exe')       # 括号内为你的MicrosoftEdge driver存放路径
+   ```
+   
+5. 如果以上以上工作你都顺利完成，那么你可以运行一遍，测试是否通过。
+
+   如果没有通过那么请仔细核对上述工作是否全部完成、若还是不能请给予我反馈。
+
+   如果顺利通过，我们进行接下来的工作
+##### 配合Windows 10自带的定时任务完成每日打卡
+
+​		虽然我们完成了每日打卡，但是每天还得手动执行一遍程序，还是有点麻烦。那么有没有自动打卡的方法呢，我们配合Windows 10 的定时任务计划即可。
+
+​		**开始菜单（鼠标右键）**-------**计算机管理**
+
+​		接下来如下图所示。
+
+​		<img src="https://i.loli.net/2020/05/06/efiWkxyd98STbqc.png" alt="T_FGVHY@19RFPHRH_F3TR_O.png" style="zoom:50%;" />
+
+python路径可以通过在命令行窗口中输入
+
+```python
+python
+import sys
+print(sys.path)
+```
+
+一般为`"C:\Users\用户名\AppData\Local\Programs\Python\"`路径下
+
+程序路径即为你的**CDUT.py** 存储路径
+
+![<img src="https://i.loli.net/2020/05/06/Fu5QhTlU2WYavrg.png" alt="img" style="zoom:72%;" />]
+
+测试一下。通过！！！
+
+#### 声明
+
+本脚本仅供学习交流使用，其余任何行为均与作者无关。
